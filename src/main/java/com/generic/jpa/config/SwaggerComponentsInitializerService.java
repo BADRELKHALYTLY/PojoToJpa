fd8476a13a4da204054a8bad01c4388f566ef559
Adam Hardoumi <adam.hardoumi@haulogy.net>
2024-05-01 03:02:03 +0200

    feature-XYZ: refactoring

--- src/main/java/com/generic/jpa/config/SwaggerComponentsInitializerService.java
+++ src/main/java/com/generic/jpa/config/SwaggerComponentsInitializerService.java
@@ -8,8 +8,7 @@ public final class SwaggerComponentsInitializerService {
 
     public static Components retrieveComponents() {
         Components components = new Components();
-        initApiResponses().entrySet().stream()
-            .forEach(entry -> components.addResponses(entry.getKey(), entry.getValue()));
+        initApiResponses().forEach(components::addResponses);
         return components;
     }
 
